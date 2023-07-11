c0a1e0cf02a49fc642c911c255dc8c5ff40483f1
barath <barath11raju@gmail.com>
2023-07-11 23:53:23 +0530

    nested routes implementation

--- App.jsx
+++ App.jsx
@@ -1,27 +1,35 @@
+import "./server"
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Layout from './components/Layout'
+
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vanslist from './pages/Vanslist';
-import VanDetail from './pages/VanDetail';
-import "./server"
+import Vanslist from './pages/vans/Vanslist';
+import VanDetail from './pages/vans/VanDetail';
+import Dashboard from './pages/host/Dashboard';
+import Income from './pages/host/Income';
+import Reviews from './pages/host/Reviews';
+import HostLayout from "./components/HostLayout";
+
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vanslist />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vanslist />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          <Route path="host" element={<HostLayout />} >
+            <Route index element={<Dashboard />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="income" element={<Income />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ pages/host/Dashboard.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom'
+
+export default function Dashboard(){
+    return (
+        <>
+        <h1>
+            Dashboard page goes here
+        </h1>
+        <Outlet />
+        </>
+    )
+}
